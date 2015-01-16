4919cb1b6a1f5492c70335bfa1818de903dd02c5
TakaakiFuruse <takaakifuruse@gmail.com>
2015-01-16 09:47:10 +0900

    start timer working, need to stop timer

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
---
+++ public/js/meditation.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function exercise (actionNum, actionArray){
+  this.actionNum = actionNum;
+  this.actionArray = actionArray;
+  this.startTime = 0;
+  this.endTime = 0;
+  this.averageTime = 0;
+  this.longestTime = 0;
+  this.shortestTime = 0;
+}
+
+exercise.prototype.incrementTimes = function() {
+  this.startTime += 1;
+  return this.startTime;
+};
+
+exercise.prototype.startCount = function() {
+  var self = this;
+  var timer = setInterval(function(){
+    $("li.time h3").html(self.incrementTimes());
+     }, 10);
+};
+
+exercise.prototype.stopCount = function() {
+  var self = this;
+  clearInterval(timer);
+  this.endTime = this.startTime;
+  return this.endTime;
+};
+
+exercise.prototype.showTime = function() {
+  // put jquery statement here....
+};
+
+exercise.prototype.sendActionData = function() {
+  // put Ajax statment here
+};
+
+exercise.prototype.recieveActionData = function() {
+  // here comes Ajax too...
+};
+
+exercise.prototype.showActoinResult = function() {
+  // put jquery statement here....
+};
