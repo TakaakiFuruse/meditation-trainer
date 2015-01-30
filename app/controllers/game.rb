498a50b710a1d496953f22314eed70a1751932ed
TakaakiFuruse <takaakifuruse@gmail.com>
2015-01-30 16:29:44 +0900

    p-coded /exercise/start route

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -9,6 +9,34 @@ get '/exercise' do
   erb :'/exercise'
 end
 
+get '/exercise/start' do
+ # * get actions in params
+ # * create action object from params, create game object
+ # * store actions in variables 
+ # * make associatoin, user << game , game << actions 
+ # * send actions to JS in view
+
+#   new_game = Game.create()
+#   login_user = User.find(session[:user_id])
+#   exercises = params[:actions].split(" ")
+
+#   exercises_object_array = exercises.each do |exercise|
+#     Action.create(name: "#{action}")
+#   end
+
+
+#   login_user.games << new_game
+
+
+# (1..5).to_a.each do |n|
+#   Game.find(1).actions <<  Action.find(n) 
+# end
+
+#   new_game.actions << exercises
+
+#   binding.pry
+end
+
 get '/exercise/results' do
   @action_result = Action.create(params)
 end
