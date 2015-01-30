80c57359148366ca4809770cc0c9e800292ac4e7
TakaakiFuruse <takaakifuruse@gmail.com>
2015-01-30 15:36:04 +0900

    changed seed file abit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,12 @@
 User.create(name: "test", password: "test", password_confirmation: "test")
 Game.create()
-Action.create(name: "act1", time: 1.11)
 
-Game.find(1).actions << Action.find(1)
+%w(action1 action2 action3 action4 action5).each do |action|
+  Action.create(name: "#{action}", time: 1.11)
+end
+
+(1..5).to_a.each do |n|
+  Game.find(1).actions <<  Action.find(n) 
+end
+
 User.find(1).games << Game.find(1)
